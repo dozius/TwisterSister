cc33c40d648158bae524d384285a88e7fba027c4
Dan Smith <dozius@gmail.com>
2021-12-31 15:06:16 -0500

    Add shift functionality to TwisterButton
    A shift button can be set which will trigger an alternate set of
    observers if it is held when a click, double click or long press
    action is triggered.

--- src/main/java/io/github/dozius/twister/TwisterButton.java
+++ src/main/java/io/github/dozius/twister/TwisterButton.java
@@ -34,7 +34,8 @@ import io.github.dozius.TwisterSisterExtension;
 /**
  * A button on the Twister. Can be either a knob button or side button.
  *
- * Provides a clicked, double clicked and long press action that can be observed.
+ * Provides a clicked, double clicked and long press action that can be observed. An optional
+ * "shift" button can be set for alternative actions when this button is held.
  */
 public class TwisterButton
 {
@@ -46,10 +47,14 @@ public class TwisterButton
   private final Set<Runnable> clickedObservers = new HashSet<>();
   private final Set<Runnable> doubleClickedObservers = new HashSet<>();
   private final Set<Runnable> longPressedObservers = new HashSet<>();
+  private final Set<Runnable> shiftClickedObservers = new HashSet<>();
+  private final Set<Runnable> shiftDoubleClickedObservers = new HashSet<>();
+  private final Set<Runnable> shiftLongPressedObservers = new HashSet<>();
   private final Timer longPressTimer;
   private final HardwareButton button;
 
   private long lastReleasedTime;
+  private TwisterButton shiftButton;
 
   /**
    * Creates a new TwisterButton.
@@ -93,11 +98,21 @@ public class TwisterButton
   }
 
   /**
+   * Sets the button that will be used for shift functionality.
+   *
+   * @param shiftButton The button to use for shift functionality.
+   */
+  public void setShiftButton(TwisterButton shiftButton)
+  {
+    this.shiftButton = shiftButton;
+  }
+
+  /**
    * Sets an observer of the double clicked action. This will then be the only observer.
    *
    * A convenience function that clears and then adds the observer.
    *
-   * @param observer Observer to add.
+   * @param observer Observer to set to double click action.
    */
   public void setDoubleClickedObserver(Runnable observer)
   {
@@ -106,9 +121,22 @@ public class TwisterButton
   }
 
   /**
+   * Sets an observer of the shift double clicked action. This will then be the only observer.
+   *
+   * A convenience function that clears and then adds the observers.
+   *
+   * @param shiftObserver Observer to set to shift double click action.
+   */
+  public void setShiftDoubleClickedObserver(Runnable shiftObserver)
+  {
+    shiftDoubleClickedObservers.clear();
+    shiftDoubleClickedObservers.add(shiftObserver);
+  }
+
+  /**
    * Adds an observer of the double clicked action.
    *
-   * @param  observer Observer to add.
+   * @param  observer Observer to add to double click action.
    *
    * @return          True if the observer was already added.
    */
@@ -117,10 +145,28 @@ public class TwisterButton
     return doubleClickedObservers.add(observer);
   }
 
+  /**
+   * Adds an observer of the shift double clicked action.
+   *
+   * @param  shiftObserver Observer to add to shift double click action.
+   *
+   * @return               True if the observer was already added.
+   */
+  public boolean addShiftDoubleClickedObserver(Runnable shiftObserver)
+  {
+    return shiftDoubleClickedObservers.add(shiftObserver);
+  }
+
   /** Clears all observers of the double clicked action. */
   public void clearDoubleClickedObservers()
   {
     doubleClickedObservers.clear();
+  }
+
+  /** Clears all observers of the shift double clicked action. */
+  public void clearShiftDoubleClickedObservers()
+  {
+    shiftDoubleClickedObservers.clear();
   }
 
   /**
@@ -128,7 +174,7 @@ public class TwisterButton
    *
    * A convenience function that clears and then adds the observer.
    *
-   * @param observer Observer to add.
+   * @param observer Observer to set to the clicked action.
    */
   public boolean setClickedObserver(Runnable observer)
   {
@@ -137,9 +183,22 @@ public class TwisterButton
   }
 
   /**
+   * Sets an observer of the shift clicked action. This will then be the only observer.
+   *
+   * A convenience function that clears and then adds the observer.
+   *
+   * @param shiftObserver Observer to set to the shift clicked action.
+   */
+  public boolean setShiftClickedObserver(Runnable shiftObserver)
+  {
+    shiftClickedObservers.clear();
+    return shiftClickedObservers.add(shiftObserver);
+  }
+
+  /**
    * Adds an observer of the clicked action.
    *
-   * @param  observer Observer to add.
+   * @param  observer Observer to add to the clicked action.
    *
    * @return          True if the observer was already added.
    */
@@ -148,10 +207,28 @@ public class TwisterButton
     return clickedObservers.add(observer);
   }
 
+  /**
+   * Adds an observer of the shift clicked action.
+   *
+   * @param  shiftObserver Observer to add to the shift clicked action.
+   *
+   * @return               True if the observer was already added.
+   */
+  public boolean addShiftClickedObserver(Runnable shiftObserver)
+  {
+    return shiftClickedObservers.add(shiftObserver);
+  }
+
   /** Clears all observers of the clicked action. */
   public void clearClickedObservers()
   {
     clickedObservers.clear();
+  }
+
+  /** Clears all observers of the shift clicked action. */
+  public void clearShiftClickedObservers()
+  {
+    shiftClickedObservers.clear();
   }
 
   /**
@@ -159,7 +236,7 @@ public class TwisterButton
    *
    * A convenience function that clears and then adds the observer.
    *
-   * @param observer Observer to add.
+   * @param observer Observer to set to the long pressed action.
    */
   public boolean setLongPressedObserver(Runnable observer)
   {
@@ -168,9 +245,22 @@ public class TwisterButton
   }
 
   /**
+   * Sets an observer of the shift long pressed action. This will then be the only observer.
+   *
+   * A convenience function that clears and then adds the observer.
+   *
+   * @param shiftObserver Observer to set to the long pressed action.
+   */
+  public boolean setShiftLongPressedObserver(Runnable shiftObserver)
+  {
+    shiftLongPressedObservers.clear();
+    return shiftLongPressedObservers.add(shiftObserver);
+  }
+
+  /**
    * Adds an observer of the long pressed action.
    *
-   * @param  observer Observer to add.
+   * @param  observer Observer to add to the long pressed action.
    *
    * @return          True if the observer was already added.
    */
@@ -179,10 +269,28 @@ public class TwisterButton
     return longPressedObservers.add(observer);
   }
 
+  /**
+   * Adds an observer of the shift long pressed action.
+   *
+   * @param  shiftObserver Observer to add to the shift long pressed action.
+   *
+   * @return               True if the observer was already added.
+   */
+  public boolean addShiftLongPressedObserver(Runnable shiftObserver)
+  {
+    return shiftLongPressedObservers.add(shiftObserver);
+  }
+
   /** Clears all observers of the long pressed action. */
   public void clearLongPressedObserver()
   {
     longPressedObservers.clear();
+  }
+
+  /** Clears all observers of the shift long pressed action. */
+  public void clearShiftLongPressedObserver()
+  {
+    shiftLongPressedObservers.clear();
   }
 
   /** Returns the buttons pressed state. */
@@ -214,23 +322,33 @@ public class TwisterButton
     }
   }
 
+  private boolean isShiftPressed()
+  {
+    if (shiftButton == null) {
+      return false;
+    }
+
+    return shiftButton.isPressed().get();
+  }
+
   private void notifyClickedObservers()
   {
-    for (Runnable observer : clickedObservers) {
+    for (Runnable observer : isShiftPressed() ? shiftClickedObservers : clickedObservers) {
       observer.run();
     }
   }
 
   private void notifyDoubleClickedObservers()
   {
-    for (Runnable observer : doubleClickedObservers) {
+    for (Runnable observer : isShiftPressed() ? shiftDoubleClickedObservers
+                                              : doubleClickedObservers) {
       observer.run();
     }
   }
 
   private void notifyLongPressedObservers()
   {
-    for (Runnable observer : longPressedObservers) {
+    for (Runnable observer : isShiftPressed() ? shiftLongPressedObservers : longPressedObservers) {
       observer.run();
     }
   }
