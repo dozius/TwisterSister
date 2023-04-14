2543604d15e8895d17c2a7a03fb8de457e951979
Dan Smith <dozius@gmail.com>
2023-04-14 18:22:59 -0400

    Bump version

--- src/main/java/io/github/dozius/TwisterSisterExtensionDefinition.java
+++ src/main/java/io/github/dozius/TwisterSisterExtensionDefinition.java
@@ -47,7 +47,7 @@ public class TwisterSisterExtensionDefinition extends ControllerExtensionDefinit
   @Override
   public String getVersion()
   {
-    return "2.0.0";
+    return "2.0.1";
   }
 
   @Override
